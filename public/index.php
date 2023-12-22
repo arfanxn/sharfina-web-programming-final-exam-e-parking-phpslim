36495083ae5527c1cae925015b7f9e08a410d30e
arfanxn <arfan2173@gmail.com>
2023-12-23 01:07:44 +0700

    Suppress deprecation notices

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+
+// Suppress deprecation notices
+error_reporting(error_reporting() & ~E_DEPRECATED);
+
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
