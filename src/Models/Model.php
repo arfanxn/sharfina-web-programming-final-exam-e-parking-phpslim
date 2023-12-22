4fad6b7cd37540dd87bdeecfaa27407e6fa4df75
arfanxn <arfan2173@gmail.com>
2023-12-23 04:18:48 +0700

    make user model

---
+++ src/Models/Model.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Stringy\Stringy;
+
+class Model
+{
+    /**
+     * this represents the table columns
+     */
+    protected array $columns;
+
+    /**
+     * this represents the columns that will be hidden when converted to an array
+     */
+    protected array $hiddenColumns;
+
+    /**
+     * hydrate fills the model with the given data
+     *
+     * @param array $data
+     * @return self
+     */
+    public function hydrate(array $data): self
+    {
+        foreach ($this->columns as $column) {
+            $camelCasedColumn = Stringy::create($column)->camelize();
+            $setterMethodName = 'set' . ucfirst($camelCasedColumn);
+            if (method_exists($this, $setterMethodName)) {
+                $value = $data[$column];
+                $this->$setterMethodName($value);
+            }
+        }
+        return $this;
+    }
+
+
+    /**
+     * toArray converts self (model) to an array
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $data = [];
+        foreach ($this->columns as $column) {
+            if (isset($hiddenColumns) && in_array($column, $this->hiddenColumns)) {
+                continue; // skip hidden columns
+            }
+
+            $camelCasedColumn = Stringy::create($column)->camelize();
+            $getterMethodName = 'get' . ucfirst($camelCasedColumn);
+
+            if (method_exists($this, $getterMethodName)) {
+                $data[$column] =  $this->$getterMethodName();
+            }
+        }
+        return $data;
+    }
+}
---
+++ src/Models/User.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Models;
+
+use DateTime;
+
+class User extends Model
+{
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private DateTime $createdAt;
+    private ?DateTime $updatedAt;
+
+    public function __construct()
+    {
+        $this->columns = ['id', 'name' , 'email', 'password', 'created_at', 'updated_at'];
+    }
+
+    /**
+     *  ----------------------------------------------------------------
+     *  Getters and setters
+     *  ----------------------------------------------------------------
+     */
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+    /**
+     * @param string $id
+     * @return void
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+    * @return string
+    */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+    * @return string
+    */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    /**
+     * @param string $password
+     * @return void
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+    * @return DateTime
+    */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+    /**
+     * @param mixed $createdAt
+     * @return void
+     */
+    public function setCreatedAt(mixed $createdAt): void
+    {
+        $this->createdAt = is_string($createdAt) ? new DateTime($createdAt) : $createdAt;
+    }
+
+    /**
+    * @return ?DateTime
+    */
+    public function getUpdatedAt(): ?DateTime
+    {
+        return $this->updatedAt;
+    }
+    /**
+     * @param mixed $updatedAt
+     * @return void
+     */
+    public function setUpdatedAt(mixed $updatedAt): void
+    {
+        $this->updatedAt = is_string($updatedAt) ? new DateTime($updatedAt) : $updatedAt;
+    }
+
+}
