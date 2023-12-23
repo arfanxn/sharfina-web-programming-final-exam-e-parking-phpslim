3a47368b8ff4faf32578b8a0190f495e5d36f1a9
arfanxn <arfan2173@gmail.com>
2023-12-23 21:20:00 +0700

    update routes

--- src/routes.php
+++ src/routes.php
@@ -2,22 +2,29 @@
 
 use App\Controllers\UserController;
 use Slim\App;
+
+/*
 use Slim\Http\Request;
 use Slim\Http\Response;
+*/
 
 return function (App $app) {
     $container = $app->getContainer();
 
+    // Add the before middlewares
     $app->add(new \App\Middlewares\ValidationFailedMiddleware());
     $app->add(new \App\Middlewares\ErrorMiddleware());
 
+    /*
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
-
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
+    */
+
+    $app->post('/api/users/login', UserController::class . ':login');
 
     $app->group('/api', function (App $app) {
         /**
@@ -25,11 +32,10 @@ return function (App $app) {
          */
         $app->group('/users', function (App $app) {
             // $app->get('', function (Request $request, Response $response) {
-            //     var_dump('hello world');
             //     return $response;
             // });
 
             $app->get('/{id}', UserController::class . ':view');
         });
-    });
+    })->add(new \Tuupola\Middleware\JwtAuthentication($container->get('settings')['jwt']));
 };
