ff4e9a46f1c4ccbcbe9d0701df5886e5d986c638
arfanxn <arfan2173@gmail.com>
2023-12-23 16:41:14 +0700

    add newForField method

--- src/Exceptions/ValidationFailedException.php
+++ src/Exceptions/ValidationFailedException.php
@@ -26,9 +26,29 @@ class ValidationFailedException extends \Exception
      */
     public static function newFromErrorBag(ErrorBag $errors): self
     {
+        $errors->add('pw', '', 'hello world');
+
         $e = new self($errors->firstOfAll()[key($errors->firstOfAll())]);
         $e->setErrors($errors);
         return $e;
+    }
+
+    /**
+     * newForField instantiates a new ValidationFailedException for a single field
+     *
+     * @param string $field  name of the field
+     * @param string $message error message of the field
+     * @return self
+     */
+    public static function newForField(string $field, string $message): self
+    {
+        $errors = new ErrorBag();
+        $errors->add(
+            $field,
+            '',
+            str_replace([':attribute', ':field'], $field, $message),
+        );
+        return self::newFromErrorBag($errors);
     }
 
 }
