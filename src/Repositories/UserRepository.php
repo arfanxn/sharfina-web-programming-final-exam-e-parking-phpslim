cf7d6f04996df2c4c4f2035121aec639789b9b24
arfanxn <arfan2173@gmail.com>
2023-12-23 20:09:55 +0700

    fix error

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -2,25 +2,42 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
+
 class UserRepository extends Repository
 {
     /**
      * find finds a user by id
      *
      * @param string $id
-     *
+     * @return ?User
      */
-    public function find(string $id)
+    public function find(string $id): ?User
     {
-        var_dump($this->connection);
-        die;
-
         $stmt = $this->connection->prepare('SELECT * FROM users WHERE id = :id');
         $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
         $stmt->execute();
 
-        $data = $stmt->fetch(\PDO::FETCH_ASSOC) ?? null;
-        var_dump($data);
+        $data = $stmt->fetch(\PDO::FETCH_ASSOC) ?: null;
+
+        return is_null($data) ? null : (new User())->hydrate($data);
+    }
+
+    /**
+     * findByEmail finds a user by email
+     *
+     * @param string $email
+     * @return ?User
+     */
+    public function findByEmail(string $email): ?User
+    {
+        $stmt = $this->connection->prepare('SELECT * FROM users WHERE email = :email');
+        $stmt->bindParam(':email', $email, \PDO::PARAM_STR);
+        $stmt->execute();
+
+        $data = $stmt->fetch(\PDO::FETCH_ASSOC) ?: null;
+
+        return is_null($data) ? null : (new User())->hydrate($data);
     }
 
 }
