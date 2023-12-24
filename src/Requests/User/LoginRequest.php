f42413d0248973177fc9e50e89ff3a4e71b3418b
arfanxn <arfan2173@gmail.com>
2023-12-24 22:25:21 +0700

    update request file

--- src/Requests/User/LoginRequest.php
+++ src/Requests/User/LoginRequest.php
@@ -7,15 +7,10 @@ use App\Requests\Request;
 
 class LoginRequest extends Request
 {
-    public string $email;
-    public string $password;
-
     public function __construct(PSRRequestInterface $psrRequest)
     {
         parent::__construct($psrRequest);
 
-        $this->email = $this->getFormData()['email'] ?? '';
-        $this->password  = $this->getFormData()['password'] ?? '';
         $this->setRules([
             'email' => 'required|email',
             'password' => 'required',
