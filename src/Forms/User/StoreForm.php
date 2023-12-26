81c8bf9e9ab422258448d3ff85631e445165be7a
arfanxn <arfan2173@gmail.com>
2023-12-26 13:32:25 +0700

    made and updated form

--- src/Forms/User/StoreForm.php
+++ src/Forms/User/StoreForm.php
@@ -2,7 +2,7 @@
 
 namespace App\Forms\User;
 
-class UpdateForm
+class StoreForm
 {
     use \App\Traits\FormTrait;
 
@@ -14,21 +14,10 @@ class UpdateForm
     public function getRules(): array
     {
         return [
-            'id' => 'required',
             'name' => 'required|min:2',
             'email' => 'required|email',
             'password' => 'required',
         ];
-    }
-
-    public function getId(): string
-    {
-        return $this->id;
-    }
-    public function setId(string $id): self
-    {
-        $this->id = $id;
-        return $this;
     }
 
     public function getName(): string
