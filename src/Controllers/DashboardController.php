49d073bfedc3b4af174e287ccb5b27798a0d4087
arfanxn <arfan2173@gmail.com>
2023-12-26 13:22:28 +0700

    update dashboard controller

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Helpers\Session;
+use App\Resources\ResponseBody;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -10,6 +11,13 @@ class DashboardController extends Controller
 {
     public function __invoke(Request $request, Response $response): Response
     {
-        return $this->getContainer()->renderer->render($response, 'dashboard.phtml', Session::pullRedirectData());
+        return $this->getContainer()->renderer->render(
+            $response,
+            'dashboard.phtml',
+            ResponseBody::new()
+                ->setStatusCode(200)
+                ->mergePayload(Session::pullRedirectData())
+                ->toArray()
+        );
     }
 }
