d6ac9f9da0a7c1e9e71b630390f6d90abd8671d2
arfanxn <arfan2173@gmail.com>
2023-12-27 02:16:23 +0700

    make user resource

---
+++ src/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Resources;
+
+use App\Interfaces\ArrayableInterface;
+use App\Models\User;
+
+class UserResource implements ArrayableInterface
+{
+    private User $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    public function toArray(): array
+    {
+        $model = $this->model;
+        return [
+            'id' => $model->getId(),
+            'name' => $model->getName(),
+            'email' => $model->getEmail(),
+            'created_at' => $model->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $model->getUpdatedAt() ? $model->getUpdatedAt()->format('Y-m-d H:i:s') : null,
+        ];
+    }
+}
